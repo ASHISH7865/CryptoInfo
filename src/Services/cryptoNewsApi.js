4a4b65a064d290bb51ac4e6f0daca4b2a9f6cfea
ASHISH <61015479+ASHISH7865@users.noreply.github.com>
2022-02-13 18:14:46 +0530

    created cryptoverse app

---
+++ src/Services/cryptoNewsApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "7a3ecf4888msha7f4346298ecc88p1286f3jsn42c90fbb7c7a",
+};
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
